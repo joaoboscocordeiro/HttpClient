46e91878f0fb86831d58d7feea858f4eb01bc844
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2025-05-01 14:32:04 -0300

    Criando models

---
+++ HttpClient/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HttpClient.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public byte[] senhaSalt { get; set; }
+        public byte[] senhaHash { get; set; }
+    }
+}
